be60ff2bc37fb09c963109beb27d2fd4a3824e8e
abord77 <raymond2003li@gmail.com>
2024-04-13 17:56:39 -0400

    added put and delete endpoints

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -2,6 +2,7 @@
 using LearningApi.Mappers;
 using LearningApi.DTOs.Stocks;
 using Microsoft.AspNetCore.Mvc;
+using LearningApi.Models;
 
 namespace LearningApi.Controllers {
     [Route("api/stock")]
@@ -13,13 +14,13 @@ namespace LearningApi.Controllers {
         }
 
         [HttpGet]
-        public IActionResult GetAll() {
+        public IActionResult GetAll() { // ep 4
             var stock = _dbContext.Stocks.ToList().Select(s => s.ToStockDTO());
             return Ok(stock);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id) {
+        public IActionResult GetById([FromRoute] int id) { // ep 4
             var stock = _dbContext.Stocks.Find(id);
 
             if (stock == null) {
@@ -29,11 +30,47 @@ namespace LearningApi.Controllers {
         }
 
         [HttpPost]
-        public IActionResult CreateNewEntry([FromBody] CreateStockRequestDto stockDto) {
+        public IActionResult CreateNewEntry([FromBody] CreateStockRequestDto stockDto) { // ep 6
             var stockModel = stockDto.ToStockFromCreateDTO();
             _dbContext.Stocks.Add(stockModel);
             _dbContext.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto) { // ep 7
+            var stockModel = _dbContext.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null) {
+                return NotFound();
+            }
+
+
+            // this takes the payload to this endpoint and modifies the id using that body
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _dbContext.SaveChanges();
+            return Ok(stockModel.ToStockDTO());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id) { // ep 8
+            var stockModel = _dbContext.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null) {
+                return NotFound();
+            }
+
+            _dbContext.Stocks.Remove(stockModel);
+            _dbContext.SaveChanges();
+            return NoContent();
         }
     }
 }
---
+++ DTOs/Stocks/UpdateStockRequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace LearningApi.DTOs.Stocks {
+    public class UpdateStockRequestDto {
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public string Industry { get; set; } = string.Empty;
+
+        public decimal Purchase { get; set; }
+
+        public decimal LastDiv { get; set; }
+
+        public long MarketCap { get; set; }
+    }
+}
