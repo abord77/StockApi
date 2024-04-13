b87f96117876cea638848efbdb596ebd5b7119eb
abord77 <raymond2003li@gmail.com>
2024-04-13 18:02:57 -0400

    changed stockController to be async

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -3,25 +3,28 @@ using LearningApi.Mappers;
 using LearningApi.DTOs.Stocks;
 using Microsoft.AspNetCore.Mvc;
 using LearningApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LearningApi.Controllers {
     [Route("api/stock")]
     [ApiController]
-    public class StockController : ControllerBase {
+    public class StockController : ControllerBase { // changing all endpoints to async is ep 9
+                                                    // note: anyuthing that needs to go out and find other information should be wrapped in await (ex. anything in this controller going to the db should be async)
         private readonly ApplicationDBContext _dbContext;
         public StockController(ApplicationDBContext dbContext) {
             _dbContext = dbContext;
         }
 
         [HttpGet]
-        public IActionResult GetAll() { // ep 4
-            var stock = _dbContext.Stocks.ToList().Select(s => s.ToStockDTO());
+        public async Task<IActionResult> GetAll() { // ep 4
+            var stock = await _dbContext.Stocks.ToListAsync();
+            var stockDto = stock.Select(s => s.ToStockDTO());
             return Ok(stock);
         }
-
+         
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id) { // ep 4
-            var stock = _dbContext.Stocks.Find(id);
+        public async Task<IActionResult> GetById([FromRoute] int id) { // ep 4
+            var stock = await _dbContext.Stocks.FindAsync(id);
 
             if (stock == null) {
                 return NotFound();
@@ -30,17 +33,17 @@ namespace LearningApi.Controllers {
         }
 
         [HttpPost]
-        public IActionResult CreateNewEntry([FromBody] CreateStockRequestDto stockDto) { // ep 6
+        public async Task<IActionResult> CreateNewEntry([FromBody] CreateStockRequestDto stockDto) { // ep 6
             var stockModel = stockDto.ToStockFromCreateDTO();
-            _dbContext.Stocks.Add(stockModel);
-            _dbContext.SaveChanges();
+            await _dbContext.Stocks.AddAsync(stockModel);
+            await _dbContext.SaveChangesAsync();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel);
         }
 
         [HttpPut]
         [Route("{id}")]
-        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto) { // ep 7
-            var stockModel = _dbContext.Stocks.FirstOrDefault(x => x.Id == id);
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto) { // ep 7
+            var stockModel = await _dbContext.Stocks.FirstOrDefaultAsync(x => x.Id == id);
 
             if (stockModel == null) {
                 return NotFound();
@@ -55,21 +58,21 @@ namespace LearningApi.Controllers {
             stockModel.Industry = updateDto.Industry;
             stockModel.MarketCap = updateDto.MarketCap;
 
-            _dbContext.SaveChanges();
+            await _dbContext.SaveChangesAsync();
             return Ok(stockModel.ToStockDTO());
         }
 
         [HttpDelete]
         [Route("{id}")]
-        public IActionResult Delete([FromRoute] int id) { // ep 8
-            var stockModel = _dbContext.Stocks.FirstOrDefault(x => x.Id == id);
+        public async Task<IActionResult> Delete([FromRoute] int id) { // ep 8
+            var stockModel = await _dbContext.Stocks.FirstOrDefaultAsync(x => x.Id == id);
 
             if (stockModel == null) {
                 return NotFound();
             }
 
-            _dbContext.Stocks.Remove(stockModel);
-            _dbContext.SaveChanges();
+            _dbContext.Stocks.Remove(stockModel); // delete is not an asynchronous function since it is simply marking it for deletion, saving the change is a real I/O operation
+            await _dbContext.SaveChangesAsync();
             return NoContent();
         }
     }
